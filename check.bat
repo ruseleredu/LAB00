78de5f51386039099cc6cdb600508a45717e2e23
Adriano Ruseler <ruseler@utfpr.edu.br>
2025-04-07 21:38:22 -0300

    System Check

---
+++ check.bat
@@ -0,0 +1,6 @@
+git -v >> git-config.txt
+git config list --show-origin >> git-config.txt
+gcc --version >> mingw-config.txt
+g++ --version >> mingw-config.txt
+gdb --version >> mingw-config.txt
+code -v >> vscode-config.txt
